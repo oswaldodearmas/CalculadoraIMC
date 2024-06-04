4221cdb80c982c93983adc529d1243fe9df62c8e
oswaldodearmas <oswaldodearmas@gmail.com>
2024-06-04 14:01:31 +0200

    Primeros pasos en Android

--- app/src/main/java/com/odearmas/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/odearmas/calculadoraimc/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.odearmas.calculadoraimc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.odearmas.calculadoraimc.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var welcomeTextView: TextView
+    lateinit var clickButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        welcomeTextView = findViewById(R.id.welcomeTextView)
+        clickButton = findViewById(R.id.clickButton)
+
+        clickButton.setOnClickListener {
+            welcomeTextView.text = getString(R.string.mainActivity_byeTextView)
+            clickButton.text = getString(R.string.mainActivity_clickedButton)
         }
     }
 }
