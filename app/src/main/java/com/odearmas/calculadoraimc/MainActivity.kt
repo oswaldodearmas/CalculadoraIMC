46c2baa7a8b93ed01cb4e0ae107aace50b67a6bc
oswaldodearmas <oswaldodearmas@gmail.com>
2024-06-06 14:03:23 +0200

    App funcional

--- app/src/main/java/com/odearmas/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/odearmas/calculadoraimc/MainActivity.kt
@@ -2,6 +2,7 @@ package com.odearmas.calculadoraimc
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,18 +13,97 @@ import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var welcomeTextView: TextView
-    lateinit var clickButton: Button
+    private lateinit var heightTextView: TextView
+    private lateinit var weightTextView: TextView
+    private lateinit var BMITextView: TextView
+    private lateinit var lessButton: Button
+    private lateinit var moreButton: Button
+    private lateinit var heightSeekBar: SeekBar
+    private lateinit var calculate: Button
+    private var currentHeight: Int = 170
+    private var currentWeight: Int = 75
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        welcomeTextView = findViewById(R.id.welcomeTextView)
-        clickButton = findViewById(R.id.clickButton)
+        initView()
+    }
 
-        clickButton.setOnClickListener {
-            welcomeTextView.text = getString(R.string.mainActivity_byeTextView)
-            clickButton.text = getString(R.string.mainActivity_clickedButton)
+    fun initView() {
+        heightTextView = findViewById(R.id.heightTextView)
+        weightTextView = findViewById(R.id.weightTextView)
+        BMITextView = findViewById(R.id.BMITextView)
+        lessButton = findViewById(R.id.lessButton)
+        moreButton = findViewById(R.id.moreButton)
+        heightSeekBar = findViewById(R.id.heightSeekBar)
+        calculate = findViewById(R.id.calculateButton)
+
+        heightSeekBar.progress = currentHeight
+        setHeightText()
+        setWeightText()
+
+        lessButton.setOnClickListener {
+            if (currentWeight > 35 && currentWeight <= 200) {
+                --currentWeight
+                setWeightText()
+            }
+        }
+        moreButton.setOnClickListener {
+            if (currentWeight >= 35 && currentWeight < 200) {
+                ++currentWeight
+                setWeightText()
+            }
+        }
+
+        heightSeekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    currentHeight = progress
+                    setHeightText()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
+                override fun onStopTrackingTouch(seekBar: SeekBar?) = Unit
+            },
+        )
+        calculate.setOnClickListener { BMITextView.text = calculateBMI() }
+
+    }
+
+
+    fun setHeightText() {
+        heightTextView.text = getString(R.string.height_Text, currentHeight.toString())
+    }
+
+    fun setWeightText() {
+        weightTextView.text = getString(R.string.weight_Text, currentWeight.toString())
+    }
+
+    fun calculateBMI(): String {
+        var BMI: Float = (currentWeight / (currentHeight * currentHeight / 10000)).toFloat()
+        when (BMI) {
+            in 0.0..18.5 -> {
+                return "BMI: $BMI\nBajo de peso"
+            }
+
+            in 18.5..24.9 -> {
+                return "BMI: $BMI\nBien de peso"
+            }
+
+            in 24.9..29.9 -> {
+                return "BMI: $BMI\nAlto peso"
+            }
+
+            else -> {
+                return "BMI: $BMI\nDemasiado peso"
+            }
+
+
         }
     }
 }
