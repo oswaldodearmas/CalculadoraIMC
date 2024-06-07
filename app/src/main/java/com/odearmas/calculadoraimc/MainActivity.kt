2f5050f719465a97fa99d87f43a649e00829ed7e
Oswaldo De Armas <oswaldodearmas@gmail.com>
2024-06-07 16:31:25 +0200

    Continuous click incorporated

--- app/src/main/java/com/odearmas/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/odearmas/calculadoraimc/MainActivity.kt
@@ -7,14 +7,12 @@ import android.util.Log
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.odearmas.calculadoraimc.*
-import kotlin.concurrent.timerTask
 import kotlin.math.pow
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,11 +25,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var lessButton: Button
     private lateinit var moreButton: Button
     private lateinit var heightSeekBar: SeekBar
-    private lateinit var calculate: Button
+    private lateinit var calculateButton: Button
     private var currentHeight: Int = 170
     private var currentWeight: Int = 75
 
+    private val handler = Handler(Looper.getMainLooper())
+    private val weightDecreaseRunnable = object : Runnable {
+        override fun run() {
+            if (currentWeight > 35 && currentWeight <= 200) {
+                --currentWeight
+                setWeightText()
+                handler.postDelayed(this, 100)
+            }
+        }
+    }
 
+    private val weightIncreaseRunnable = object : Runnable {
+        override fun run() {
+            if (currentWeight >= 35 && currentWeight < 200) {
+                ++currentWeight
+                setWeightText()
+                handler.postDelayed(this, 100)
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -40,6 +57,7 @@ class MainActivity : AppCompatActivity() {
         Log.d("FIN_CREATE","Finaliza la ejecución del Create")
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun initView() {
         Log.d("INICIO_INIT","Se inicia la ejecución del Init")
         heightTextView = findViewById(R.id.heightTextView)
@@ -49,18 +67,47 @@ class MainActivity : AppCompatActivity() {
         lessButton = findViewById(R.id.lessButton)
         moreButton = findViewById(R.id.moreButton)
         heightSeekBar = findViewById(R.id.heightSeekBar)
-        calculate = findViewById(R.id.calculateButton)
+        calculateButton = findViewById(R.id.calculateButton)
 
         heightSeekBar.progress = currentHeight
         setHeightText()
         setWeightText()
 
-        lessButton.setOnClickListener {
-            if (currentWeight > 35 && currentWeight <= 200) {
-                --currentWeight
-                setWeightText()
+        this.lessButton.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    handler.post(weightDecreaseRunnable)
+                    true
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    handler.removeCallbacks(weightDecreaseRunnable)
+                    true
+                }
+                else -> false
             }
         }
+
+        this.moreButton.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    handler.post(weightIncreaseRunnable)
+                    true
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    handler.removeCallbacks(weightIncreaseRunnable)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+//        lessButton.setOnClickListener {
+//            if (currentWeight > 35 && currentWeight <= 200) {
+//                --currentWeight
+//                setWeightText()
+//            }
+//        }
         moreButton.setOnClickListener {
             if (currentWeight >= 35 && currentWeight < 200) {
                 ++currentWeight
@@ -83,7 +130,7 @@ class MainActivity : AppCompatActivity() {
                 override fun onStopTrackingTouch(seekBar: SeekBar?) = Unit
             },
         )
-        calculate.setOnClickListener { BMITextView.text = calculateBMI() }
+        calculateButton.setOnClickListener { BMITextView.text = calculateBMI() }
 
         Log.d("FIN_INIT","Finaliza la ejecución del Init")
     }
